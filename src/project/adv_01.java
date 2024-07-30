7d7693a65145abfb4c13863745ecf8539cbb83c1
sandeep <sandeepjakkula6466@gmial.com>
2024-07-30 18:53:45 +0530

    initial commit

---
+++ src/project/adv_01.java
@@ -0,0 +1,30 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class adv_01 {
+	public static void main(String[] args) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","System","tiger");
+			Statement stm=con.createStatement();
+			ResultSet rs =stm.executeQuery("select * from book");
+			System.out.println("4");
+			System.out.println(" user details ....");
+			while (rs.next())
+			{
+				System.out.println(rs.getLong(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getInt(4)+"\t"+rs.getInt(5));
+			}
+			con.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		//System.out.println(e.toString());
+		}
+	}
+
+}
